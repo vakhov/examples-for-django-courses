0c373677a03fee4db6403e08e69d7f855e7c2741
Alexander Vakhov <alexander@vakhov.ru>
2019-10-14 20:55:32 +0500

    [change] Remove pycache files. Add template and static files

--- blog/views.py
+++ blog/views.py
@@ -10,4 +10,5 @@ def post_list(request):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
-                             publish_day=day)
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
