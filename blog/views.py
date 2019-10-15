286743b45047257a1feba3538824ff526860d3a1
Alexander Vakhov <alexander@vakhov.ru>
2019-10-15 22:04:44 +0500

    [add] form send post with email

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 
 from blog.models import Post
+from forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -15,3 +16,18 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Получение статьи по идентификатору
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        # Форма была отправлена на сохранение
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Все поля пормы прошли валидацию
+            cd = form.cleaned_data
+            # отправка Электронной почты
+    else:
+        form = EmailPostForm()
+        return render(request, 'blog/post/share.html', {'post': post, 'form': form})
